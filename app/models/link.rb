d2c1e00869c1e0cd31b8beb16a708eba2245a20e
gntics <alexandre.quard@gmail.com>
2015-02-15 19:54:03 +0100

    change Link model to have a slug field

--- app/models/link.rb
+++ app/models/link.rb
@@ -2,8 +2,23 @@ class Link
   include Mongoid::Document
   include Mongoid::Timestamps
 
+  before_save :generate_slug
+
   embedded_in :user, inverse_of: :links
 
   field :original_url, type: String, default: ""
-  field :short_url,    type: String, default: ""
+  field :slug,         type: String, default: ""
+
+  validates :original_url, presence: true
+
+  def short_url
+    "#{ENV.fetch('HOSTNAME')}#{slug}"
+  end
+
+  private
+
+  def generate_slug
+    self.slug = "#{id}"
+  end
+
 end
